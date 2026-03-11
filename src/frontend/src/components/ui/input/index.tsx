9c6dee66c02f1a95f716bdc6610388f85da8ae17
Artur.aax <chainsawofgame@gmail.com>
2026-03-11 10:51:36 -0300

    Delete src/frontend directory

--- src/frontend/src/components/ui/input/index.tsx
+++
@@ -1,7 +0,0 @@
-import type { InputHTMLAttributes } from "react";
-import './style.css';
-
-export function Input(props: InputHTMLAttributes<HTMLInputElement>) {
-    return <input className='input' { ...props } />;
-}
-
